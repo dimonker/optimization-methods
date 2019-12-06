2ed497696d0334ad119c920c86fcf367694e3be5
Ilya <vinnikov_i@mail.ru>
2019-12-06 23:52:08 +0300

    :)

--- lab1/gradient_descent.js
+++ lab1/gradient_descent.js
@@ -1,19 +1,26 @@
-const gradientDescent = (f1, f2, x0, x1, precision, gamma) => {
+const gradientDescent = (df1, df2, x0, x1, precision, gamma) => {
+  let F = (a, b) => 100*Math.pow(b-Math.pow(a,2),2)+Math.pow((1-a),2)
+  // let F = (a, b) => Math.pow(Math.E,a+b)+2*Math.pow(b,2)+Math.pow(a,2)
   let curX0 = x0;
   let curX1 = x1;
   let x0PreviousStepSize = 2 * precision;
   let x1PreviousStepSize = 2 * precision;
+  let fPrevSize = 1
 
-
-
-  while (x0PreviousStepSize > precision && x1PreviousStepSize > precision) {
+  while ( (fPrevSize > precision) &&  (x0PreviousStepSize > precision && x1PreviousStepSize > precision)) {
+      
       let prevX0 = curX0;
       let prevX1 = curX1;
-      curX0 -= gamma * f1(prevX0, prevX1);
-      curX1 -= gamma * f2(prevX0, prevX1);
+      curX0 -= gamma * df1(prevX0, prevX1);
+      curX1 -= gamma * df2(prevX0, prevX1);
+
       x0PreviousStepSize = Math.abs(curX0 - prevX0);
       x1PreviousStepSize = Math.abs(curX1 - prevX1);
-      console.log(curX0+" "+curX1);
+      fPrevSize = Math.abs(F(curX0, curX1)-F(prevX0, prevX1));
+      // console.log(grad(0,0));
+      // console.log(curX0+" "+curX1);
+      console.log(F(curX0, curX1))
+
   }
 
   return {
@@ -22,9 +29,20 @@ const gradientDescent = (f1, f2, x0, x1, precision, gamma) => {
   }
 }
 
-let df1 = (x0, x1) => 400*x0*(Math.pow(x0, 2)+x1)-2*(1-x0);
-let df2 = (x0, x1) => 200*(x1+(Math.pow(x0, 2)));
+let df1 = (x1, x2) => 400*Math.pow(x1, 3)+(2-400*x2)*x1-2;
+let df2 = (x1, x2) => 200*(x2-Math.pow(x1, 2));
 
-let res = gradientDescent(df1, df2, -50, -55, 0.01, 0.000001);
 
-export {gradientDescent}
+// let df1 = (x0, x1) => Math.pow(Math.E, x0+x1)+2*x0;
+// let df2 = (x0, x1) => Math.pow(Math.E, x0+x1)+4*x1;
+
+let grad = (x1, x2) => df1(x1,x2) + df2(x1,x2)
+
+console.log(grad(0,0))
+
+// let res = gradientDescent(grad, 1.3, 1.3, 0.00000001, 0.00001);
+
+let res = gradientDescent(df1, df2, 7, 7, 0.00000001, 0.00010);
+console.log(res);
+
+// export {gradientDescent}
